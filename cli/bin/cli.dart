dacaaa84ed7121dfdbc2deb3b538297333377b48
Iván Santos <IvanSantosgonz@gmail.com>
2022-12-27 10:15:25 +0000

    initial commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,5 @@
+import 'package:cli/quarter-calculator.dart' as cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli.calculate()}!');
+}
---
+++ cli/test/cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:cli/quarter-calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
