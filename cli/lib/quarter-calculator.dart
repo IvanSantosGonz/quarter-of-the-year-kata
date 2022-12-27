e2163a363fe4ed7245046688d7f203836880a374
Iván Santos <IvanSantosgonz@gmail.com>
2022-12-27 10:46:36 +0000

    GREEN: for month 1 the output quarter should be 1

--- cli/lib/quarter-calculator.dart
+++ cli/lib/quarter-calculator.dart
@@ -1,4 +1,5 @@
 
 int quarter(int month) {
+  if (month >= 1 && month <= 12 ) return 1;
   throw ArgumentError("Input month should be between 1 and 12");
 }
--- cli/test/cli_test.dart
+++ cli/test/cli_test.dart
@@ -6,4 +6,8 @@ void main() {
   test('the input month should be valid (between 1 and 12)', () {
     expect(() => quarter(0), throwsArgumentError);
   });
+
+  test('for month 1 the output should be 1', () {
+    expect(quarter(1), 1);
+  });
 }
